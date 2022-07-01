ad2855a5e0ad7bfe1c60288d3bc018574f867be8
Harald Judt <h.judt@gmx.at>
2022-07-01 22:46:39 +0200

    Add package information and GPL-3 license

--- find-duplicates.el
+++ find-duplicates.el
@@ -1,5 +1,47 @@
 ;;; find-duplicates.el --- Find duplicate files locally and remotely  -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2022 Harald Judt
+
+;; Author: Harald Judt <h.judt@gmx.at>
+;; Maintainer: Harald Judt <h.judt@gmx.at>
+;; Created: 2022
+;; Version: 0.1
+;; Package-Requires: ((emacs "28.1"))
+;; Keywords: files
+;; Homepage: https://codeberg.org/hjudt/find-duplicates
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package helps to find duplicate files on local and remote filesystems.
+;; It is similar to the fdupes command-line utility but written in Emacs Lisp
+;; and should also work on every remote filesystem that TRAMP supports and
+;; where executable commands can be called remotely.  The only external
+;; requirement is a checksum program like md5 or sha256sum that generates a
+;; hash value from the contents of a file used for comparison, because Emacs
+;; cannot do that in a performance-efficient way.
+;;
+;; find-duplicates works by first searching files of the same size, then
+;; invoking the calculation of the checksum for these files, and presents the
+;; grouped results in a Dired buffer that the user can work with similarly to
+;; a regular Dired buffer.
+
+;;; Code:
+
 (require 'cl-lib)
 (require 'dired)
 

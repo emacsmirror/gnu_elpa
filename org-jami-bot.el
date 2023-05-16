76d6d76871c11b22e72b963db3f72eec4d6213fd
Hanno Perrey <hanno@hoowl.se>
2023-05-16 20:25:35 +0200

    fixes typo in package description

--- org-jami-bot.el
+++ org-jami-bot.el
@@ -33,10 +33,10 @@
 ;; items at specific dates, compose multi-measure captures and capture images --
 ;; all by sending a message via the GNU Jami messenger.
 ;;
-;; `org-jami-bot' multi-message capture from within the Jami messenger app --
-;; that is, a capture process that consists of several messages and can include
-;; even images and other files. The process is started by sending the command
-;; "!start" followed by the title of the capture and finished by sending
+;; `org-jami-bot' provides multi-message capture from within the Jami messenger
+;; app -- that is, a capture process that consists of several messages and can
+;; include even images and other files. The process is started by sending the
+;; command "!start" followed by the title of the capture and finished by sending
 ;; "!done". Once the multi-message capture session is started, every following
 ;; message is simply added. This includes images which will be downloaded and
 ;; stored locally. A reference in the form of a link will be included in the

12a50aa425278d5982364b0f97e7a9f13ac52fe9
Emanuel Berg <moasenwood@zoho.eu>
2023-10-02 20:49:17 +0200

    Mon Oct  2 08:49:17 PM CEST 2023

--- wrap-search.el
+++ wrap-search.el
@@ -1,11 +1,28 @@
 ;;; wrap-search.el --- wrapped, non-incremental search -*- lexical-binding: t -*-
 ;;
+;; Copyright (C) 2023 Free Software Foundation, Inc.
+;;
 ;; Author: Emanuel Berg <incal@dataswamp.org>
 ;; Created: 2021-05-23
 ;; Keywords: matching
 ;; License: GPL3+
 ;; URL: https://dataswamp.org/~incal/emacs-init/wrap-search.el
-;; Version: 4.10.10
+;; Version: 4.12.10
+;;
+;; This file is part of GNU Emacs.
+;;
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;;

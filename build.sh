9ca25ec5ca871f04b4704b39fb3ba129694e02ac
Stephen Leake <stephen_leake@stephe-leake.org>
2023-11-15 09:08:48 -0800

    Release version 1.0.5; packaging fix
    * NEWS: Version.
    * README:
    * build.sh:
    * gpr-mode.el:

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Build executables for gpr mode.
-#    build.sh <other gprbuild options>
+#    build.sh options when not using Alire; <other gprbuild options>
 #    e.g. 'build.sh -j0' : use all available processors to compile
 #         'build.sh -wn' : treat warnings as warnings.
 #         'build.sh -vh' : Verbose output (high verbosity)
@@ -8,15 +8,17 @@
 # See install.sh for install
 
 if type alr; then
+    # alr can be installed from https://alire.ada.dev/
     echo "building gpr-mode executables via Alire"
-    alr get emacs_gpr_mode~1.0.4
+
+    alr get emacs_gpr_mode~1.0.5
     cd emacs_gpr_mode_*; alr build --release
 
 elif type gprbuild; then
     echo "building gpr-mode executables via gnat compiler"
     
-    if [ -d ../wisi-4.3.? ]; then
-        WISI_DIR=`ls -d ../wisi-4.3.?`
+    if [ -d ../wisi-4.3.0 ]; then
+        WISI_DIR=`ls -d ../wisi-4.3.*`
     fi
 
     args=`echo -DELPA="yes" $WISI_DIR/wisi.gpr.gp $WISI_DIR/wisi.gpr`
@@ -31,6 +33,8 @@ elif type gprbuild; then
 
     gprclean -r -P gpr_mode_wisi_parse.gpr -aP$WISI_DIR
 
+    # We don't generate the parser code here (unlike ada-mode),
+    # because it is small enough to keep in ELPA
     gprbuild -p -j8 -P gpr_mode_wisi_parse.gpr -aP $WISI_DIR "$@"
 
 else

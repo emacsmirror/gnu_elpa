7bd1956335f2f5db6a8a8e443bd735c9a561c776
Stefan Kangas <stefankangas@gmail.com>
2024-01-13 10:52:53 +0100

    ; Depend on Emacs 24.3 for lexical-binding and cl-lib

--- adjust-parens.el
+++ adjust-parens.el
@@ -4,6 +4,7 @@
 
 ;; Author: Barry O'Reilly <gundaetiapo@gmail.com>
 ;; Version: 3.1
+;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

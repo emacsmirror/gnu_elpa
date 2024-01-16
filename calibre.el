7fe464d21895c4cde2566c3e707c1d30b0782089
Kjartan Oli Agustsson <kjartanoli@disroot.org>
2024-01-16 16:50:51 +0000

    Bump version number
    * calibre.el:
    * doc/version.texi:
    
      Bump version number.

--- calibre.el
+++ calibre.el
@@ -4,7 +4,7 @@
 
 ;; Author: Kjartan Oli Agustsson <kjartanoli@disroot.org>
 ;; Maintainer: Kjartan Oli Agustsson <kjartanoli@disroot.org>
-;; Version: 1.3.4
+;; Version: 1.4.0
 ;; Package-Requires: ((emacs "27.1") (compat "29.1.4.1"))
 ;; URL: https://git.disroot.org/kjartanoli/calibre.el
 

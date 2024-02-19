3f433899ce9ce6a3d6477ae7c9362c8c202f04fb
JSDurand <mmemmew@gmail.com>
2024-02-19 12:02:14 +0800

    Bump version to 0.3.

--- ilist.el
+++ ilist.el
@@ -5,7 +5,7 @@
 ;; Author: Durand <mmemmew@gmail.com>
 ;; Keywords: convenience
 ;; URL: https://gitlab.com/mmemmew/ilist
-;; Version: 0.2
+;; Version: 0.3
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

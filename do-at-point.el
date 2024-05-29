0ddf6edb44913c170fbe23aea2ccd9ed677391db
Philip Kaludercic <philipk@posteo.net>
2024-05-29 23:04:40 +0200

    Pass the prefix arg to the transpose actions

--- do-at-point.el
+++ do-at-point.el
@@ -130,19 +130,19 @@ of this variable.")
     (word
      (?$ "Spell check" ,(lambda () (ispell-word)))
      (?d "Dictionary" ,#'dictionary-search)
-     (?t "Transpose" ,(lambda () (transpose-words 1))))
+     (?t "Transpose" ,(lambda () (transpose-words (or current-prefix-arg 1)))))
     (symbol
      (?. "Xref" ,#'xref-find-definitions)
      (?o "Occur" ,(lambda (str)
                     (occur (concat "\\_<\\(" (regexp-quote str) "\\)\\_>")))))
     (string)
     (sexp
-     (?t "Transpose" ,(lambda () (transpose-sexps 1))))
+     (?t "Transpose" ,(lambda () (transpose-sexps (or current-prefix-arg 1)))))
     (line
-     (?t "Transpose" ,(lambda () (transpose-lines 1))))
+     (?t "Transpose" ,(lambda () (transpose-lines (or current-prefix-arg 1)))))
     (paragraph
      (?$)
-     (?t "Transpose" ,(lambda () (transpose-paragraphs 1))))
+     (?t "Transpose" ,(lambda () (transpose-paragraphs (or current-prefix-arg 1)))))
     (defun
         (?e "Evaluate" ,(lambda () (eval-defun nil)))))
   "Association of things and their respective actions.

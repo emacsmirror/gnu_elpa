4026699be9609ef086614e5a27ea10f4794348ca
Michelangelo Rodriguez <michelangelo.rodriguez@gmail.com>
2024-05-31 03:24:14 +0200

    greader version 0.9.24
    URL header added.

--- greader.el
+++ greader.el
@@ -5,8 +5,8 @@
 ;; package-requires: ((emacs "25"))
 ;; Author: Michelangelo Rodriguez <michelangelo.rodriguez@gmail.com>
 ;; Keywords: tools, accessibility
-
-;; Version: 0.9.23
+;; URL: https://www.gitlab.com/michelangelo-rodriguez/greader
+;; Version: 0.9.24
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

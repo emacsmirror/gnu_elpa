d51fa370a95eda45d142f97d45fa6e5f77b85cc3
Adam Porter <adam@alphapapa.net>
2024-06-13 00:34:20 -0500

    Set css-indent-offset to 2
    This ensures consistent indentation of our CSS files across
    developers (the default not being what this project uses).
    
    Also, reindent a couple of blocks in layout.css accordingly.
    
    (This change was made with "M-x add-dir-local-variable", which also
    added the "no-byte-compile" line and reordered the modes.)
    
    * .dir-locals.el: (css-mode) Set css-indent-offset to 2.
    * html/layout.css: Reindent two blocks.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,17 +1,18 @@
-;;; Directory Local Variables
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((tab-width . 8)
          (sentence-end-double-space . t)
          (fill-column . 70)
          (bug-reference-url-format . "https://debbugs.gnu.org/%s")))
- (log-edit-mode . ((log-edit-font-lock-gnu-style . t)
-                   (log-edit-setup-add-author . t)))
  (change-log-mode . ((add-log-time-zone-rule . t)
                      (fill-column . 74)
                      (mode . bug-reference)))
+ (css-mode . ((css-indent-offset . 2)))
  (diff-mode . ((mode . whitespace)))
  (lisp-data-mode . ((indent-tabs-mode . nil)
                     (electric-quote-comment . nil)
                     (electric-quote-string . nil)
-                    (mode . bug-reference-prog))))
+                    (mode . bug-reference-prog)))
+ (log-edit-mode . ((log-edit-font-lock-gnu-style . t)
+                   (log-edit-setup-add-author . t))))

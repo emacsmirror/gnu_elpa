ee1957f3bce6ed04627b985e95a17db190781e06
Paul Nelson <ultrono@gmail.com>
2024-06-17 17:48:20 -0700

    bump version to 0.1

--- tex-item.el
+++ tex-item.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2024  Free Software Foundation, Inc.
 
 ;; Author: Paul D. Nelson <nelson.paul.david@gmail.com>
-;; Version: 0.0
+;; Version: 0.1
 ;; URL: https://github.com/ultronozm/tex-item.el
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: tex, convenience

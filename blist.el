f5751add5626ad437459e484b66219952287c517
JSDurand <mmemmew@gmail.com>
2024-08-07 12:02:02 +0800

    Bump version to 0.4.

--- blist.el
+++ blist.el
@@ -6,7 +6,7 @@
 ;; Keywords: convenience
 ;; URL: https://gitlab.com/mmemmew/blist
 ;; Package-Requires: ((ilist "0.4") (emacs "24"))
-;; Version: 0.3
+;; Version: 0.4
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by

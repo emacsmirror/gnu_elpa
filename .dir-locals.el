3df135ce06511916d7a3c3d7c3f5705b36933aaf
aurtzy <aurtzy@gmail.com>
2024-11-04 20:57:04 -0500

    Update .dir-locals.el to use new specifications

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,10 +6,12 @@
   ((disproject-compile-suffixes
     .
     (("p i" "[Re-]Initialize a local Guix profile"
-      "[ -e .guix-profile ] && rm ./.guix-profile; \
+      :command-type compile
+      :command "[ -e .guix-profile ] && rm ./.guix-profile; \
 guix time-machine --channels=channels.scm -- \
 shell --manifest=manifest.scm --root=./.guix-profile --search-paths"
       :identifier "profile")
      ("p r" "Run minimum-supported Emacs"
-      "guix shell --pure --profile=./.guix-profile -- emacs -Q"
+      :command-type compile
+      :command "guix shell --pure --profile=./.guix-profile -- emacs -Q"
       :identifier "profile"))))))

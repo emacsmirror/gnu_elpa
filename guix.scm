bf874b1d2daf7a3f771f15a2bb9474b74e692f54
aurtzy <aurtzy@gmail.com>
2024-12-11 14:30:21 -0500

    guix: Use updated emacs-transient

--- guix.scm
+++ guix.scm
@@ -10,24 +10,6 @@
 
 (define repository-root-directory (dirname (current-filename)))
 
-(define-public emacs-transient/newer
-  (package/inherit emacs-transient
-    (name "emacs-transient")
-    (version "0.8.0")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/magit/transient")
-                    (commit (string-append "v" version))))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "1f8w6b242ar0538pimnrf7a5j277q80z7n379hyihdblvjk90xi2"))))))
-
-;; Disproject depends on emacs-transient>=0.8.0 which is not yet available
-;; upstream.  emacs-transient/newer may be used, but needs to replace
-;; emacs-transient in all other emacs packages for the environment as well to
-;; reliably work.
 (define-public emacs-disproject
   (package
     (name "emacs-disproject")
@@ -37,7 +19,7 @@
                  #:recursive? #t
                  #:select? (git-predicate repository-root-directory)))
     (build-system emacs-build-system)
-    (propagated-inputs (list emacs-transient/newer))
+    (propagated-inputs (list emacs-transient))
     (home-page "https://github.com/aurtzy/disproject")
     (synopsis "Transient interface for managing and interacting with projects")
     (description

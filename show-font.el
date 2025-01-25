1274efa47217c45a86124875080da1a51d8dc12e
Protesilaos Stavrou <info@protesilaos.com>
2025-01-25 09:39:00 +0200

    Update the copyright years

--- show-font.el
+++ show-font.el
@@ -1,6 +1,6 @@
 ;;; show-font.el --- Show font features in a buffer -*- lexical-binding: t -*-
 
-;; Copyright (C) 2024  Free Software Foundation, Inc.
+;; Copyright (C) 2024-2025  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>

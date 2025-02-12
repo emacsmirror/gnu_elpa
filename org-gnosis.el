ab0ea270a99fc2a10592792e242812e46a6f37b5
Thanos Apollo <public@thanosapollo.org>
2025-02-12 05:17:14 +0200

    version bump: 0.0.3
    * Add gpg encryption support.
    * Improve support for inserting heading tags & filetags, using
      completions.
    * Minor improvements in error handling.

--- org-gnosis.el
+++ org-gnosis.el
@@ -5,7 +5,7 @@
 ;; Author: Thanos Apollo <public@thanosapollo.org>
 ;; Keywords: extensions
 ;; URL: https://thanosapollo.org/projects/org-gnosis/
-;; Version: 0.0.2
+;; Version: 0.0.3
 
 ;; Package-Requires: ((emacs "27.2") (emacsql "4.0.0") (compat "29.1.4.2"))
 

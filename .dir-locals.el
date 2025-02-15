c0cdd058466bc54fa2dcba2eb9c48d4921fad54b
aurtzy <aurtzy@gmail.com>
2025-02-15 16:36:25 -0500

    dir-locals: Use transient suffix specifications for custom suffixes

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,50 +5,30 @@
   .
   ((disproject-custom-suffixes
     .
-    (("t c" "Time-machine: Compile to Guix profile"
-      :command-type run
-      :command (lambda () (interactive)
-                 (let ((command "\
+    (("t c" "Time-machine: Compile to Guix profile" disproject-compile
+      :cmd "\
 profile=.time-machine-guix-profile
 [ -e $profile ] && rm $profile
 guix time-machine --channels=channels.scm -- \\
 	shell --development --file=guix.scm --file=guix.scm \\
 	--manifest=manifest.scm emacs \\
-	--root=$profile --search-paths"))
-                   (if current-prefix-arg
-                       (read-shell-command "Command: " command)
-                     command)))
-      :identifier "time-machine-profile")
-     ("t r" "Time-machine: Run Emacs"
-      :command-type run
-      :command (lambda () (interactive)
-                 (let ((command "\
+	--root=$profile --search-paths"
+      :buffer-id "time-machine-profile")
+     ("t r" "Time-machine: Run Emacs" disproject-compile
+      :cmd "\
 guix shell --pure --profile=.time-machine-guix-profile -- \\
-	emacs --no-init-file --eval \"(require 'disproject)\""))
-                   (if current-prefix-arg
-                       (read-shell-command "Command: " command)
-                     command)))
-      :identifier "time-machine-profile")
-     ("l c" "Latest: Compile to Guix profile"
-      :command-type run
-      :command (lambda () (interactive)
-                 (let ((command "\
+	emacs --no-init-file --eval \"(require 'disproject)\""
+      :buffer-id "time-machine-profile")
+     ("l c" "Latest: Compile to Guix profile" disproject-compile
+      :cmd "\
 profile=.latest-guix-profile
 [ -e $profile ] && rm $profile
 guix shell --development --file=guix.scm --file=guix.scm \\
 	--manifest=manifest.scm emacs-next \\
-	--root=$profile --search-paths"))
-                   (if current-prefix-arg
-                       (read-shell-command "Command: " command)
-                     command)))
-      :identifier "latest-profile")
-     ("l r" "Latest: Run Emacs"
-      :command-type run
-      :command (lambda () (interactive)
-                 (let ((command "\
+	--root=$profile --search-paths"
+      :buffer-id "latest-profile")
+     ("l r" "Latest: Run Emacs" disproject-compile
+      :cmd "\
 guix shell --pure --profile=.latest-guix-profile -- \\
-	emacs --no-init-file --eval \"(require 'disproject)\""))
-                   (if current-prefix-arg
-                       (read-shell-command "Command: " command)
-                     command)))
-      :identifier "latest-profile"))))))
+	emacs --no-init-file --eval \"(require 'disproject)\""
+      :buffer-id "latest-profile"))))))

98125cb35c5d86226d97d52775bfa7cb1daf77a9
aurtzy <aurtzy@gmail.com>
2025-02-20 18:44:19 -0500

    dir-locals: Organize suffixes into groups

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,30 +5,32 @@
   .
   ((disproject-custom-suffixes
     .
-    (("t c" "Time-machine: Compile to Guix profile" disproject-compile
-      :cmd "\
+    (["From pinned channels"
+      ("t c" "Compile" disproject-compile
+       :cmd "\
 profile=.time-machine-guix-profile
 [ -e $profile ] && rm $profile
 guix time-machine --channels=channels.scm -- \\
 	shell --development --file=guix.scm --file=guix.scm \\
 	--manifest=manifest.scm emacs \\
 	--root=$profile --search-paths"
-      :buffer-id "time-machine-profile")
-     ("t r" "Time-machine: Run Emacs" disproject-compile
-      :cmd "\
+       :buffer-id "time-machine-profile")
+      ("t r" "Run" disproject-compile
+       :cmd "\
 guix shell --pure --profile=.time-machine-guix-profile -- \\
 	emacs --no-init-file --eval \"(require 'disproject)\""
-      :buffer-id "time-machine-profile")
-     ("l c" "Latest: Compile to Guix profile" disproject-compile
-      :cmd "\
+       :buffer-id "time-machine-profile")]
+     ["Latest"
+      ("l c" "Compile" disproject-compile
+       :cmd "\
 profile=.latest-guix-profile
 [ -e $profile ] && rm $profile
 guix shell --development --file=guix.scm --file=guix.scm \\
 	--manifest=manifest.scm emacs-next \\
 	--root=$profile --search-paths"
-      :buffer-id "latest-profile")
-     ("l r" "Latest: Run Emacs" disproject-compile
-      :cmd "\
+       :buffer-id "latest-profile")
+      ("l r" "Run" disproject-compile
+       :cmd "\
 guix shell --pure --profile=.latest-guix-profile -- \\
 	emacs --no-init-file --eval \"(require 'disproject)\""
-      :buffer-id "latest-profile"))))))
+       :buffer-id "latest-profile")])))))

203eab05ad800a30a3c43af0856c17f1ffbc15ca
c-alpha <c-alpha@noreply.codeberg.org>
2025-03-05 19:17:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Hugoista - Manage Hugo Posts like a Barista (in Emacs)
+
+![screenshot](/screenshot.png "screenshot")
+
+Hugoista is an [Emacs](https://www.gnu.org/software/emacs/) package
+which provides an overview of the blog posts in a
+[Hugo](https://gohugo.io) site, grouped by their publication status.
+Multiple Hugoista buffers can be open at the same time, each for a
+different Hugo site.
+
+To open the list of posts for a site, do `M-: (hugoista
+"~/path/to/site")`. When you have a single site only, set the variable
+`hugoista-site-dir` to the site directory (where `hugo.toml` is), and
+simply do `M-x hugoista`.
+
+With the cursor on the line for a post, hitting the `RETURN` key opens
+the corresponding file for editing. Hitting the `N` key creates a new
+post file, prompting for the file name. All standard
+`tabulated-list-mode` key bindings are available, too (for example `g`
+to refresh the list, `S` to sort by the current column, etc.).
+
+Too see Hugoista's customisation options, do `M-x customize-group RET
+hugoista RET`.
+
+Under the hood, Hugoista calls Hugo to list the posts, and create new
+content files. You are thus free to use page bundles, and all other
+features supported by Hugo, as well as all content file, and front
+matter formats supported by Hugo.
+
+Hope you'll find this useful, and happy posting!
---
+++ hugoista.el
@@ -0,0 +1,328 @@
+;;; hugoista.el --- Manage Hugo posts like a barista -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025  Free Software Foundation, Inc.
+
+;; Author: Alexander Adolf <alexander.adolf@condition-alpha.com>
+;; Maintainer: Alexander Adolf <alexander.adolf@condition-alpha.com>
+;; Version: 0.1.0
+;; Package-Requires: (cl-lib seq tabulated-list parse-csv)
+;; Homepage: https://codeberg.org/c-alpha/hugoista
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package is intended to help in curating and authoring blog
+;; posts in a Hugo-powered website.  It displays the posts in a list
+;; view, grouped by their publication status (draft, scheduled,
+;; published, and expired).  Within each status group, posts can be
+;; sorted by date, publication date, expiration date, or title.  An
+;; existing post can be edited, or a new post created using single-key
+;; shortcuts.
+
+;; To show the list of posts for a site, call the function `hugoista'.
+;; It accepts an optional argument, which is the site directory (that
+;; is, the directory where the `hugo.toml' file for the site is).
+;; Each hugoista buffer references it own site directory.  It is thus
+;; possible to have several hugoista buffers open at the same time,
+;; each for a different site.  As a convenience for when a single site
+;; is to be managed only, the optional argument to `hugoista' can be
+;; omitted, in which case the directory indicated by
+;; `hugoista-site-dir' will be used as the default site directory.
+
+;; Two operations are provided to act on individual posts.
+;; `hugoista-visit-post' (bound to RET by default), and
+;; `hugoista-new-post' (bound to N by default).  Further operations
+;; (for example renaming, or deleting post files) can conveniently be
+;; performed by other built-in facilities, such as for example `dired'.
+
+;;; Code:
+
+(require 'cl-lib)
+(require 'seq)
+(require 'tabulated-list)
+(require 'parse-csv)
+
+;;;; Customisation Variables
+
+(defgroup hugoista nil
+  "Managing Hugo posts."
+  :group 'external)
+
+(defcustom hugoista-hugo-command "hugo"
+  "Command to use the Hugo command line interface."
+  :group 'hugoista
+  :type 'string)
+
+(defcustom hugoista-site-dir "~/"
+  "Default site directory to be used by `hugoista'."
+  :group 'hugoista
+  :type 'directory
+  :local t)
+
+(defcustom hugoista-posts-dir "posts"
+  "Hugo content directory containing the blog posts."
+  :group 'hugoista
+  :type 'string
+  :local t)
+
+(defcustom hugoista-initial-sort t
+  "Sort order for newly created `hugoista' buffers."
+  :group 'hugoista
+  :type '(choice (const :tag "descending" t)
+                 (const :tag "ascending" nil)))
+
+;;;; Internal Stuff
+
+(cl-defstruct post-desc
+  "Structure containing information about an individual post."
+  path slug title date expiryDate publishDate draft permalink kind section)
+
+(defun hugoista--list-content (&optional restrict)
+  "Obtain a list of site content from Hugo.
+
+The set of content returned can optionally be constrained via RESTRICT,
+which is a cons of the form (KIND . SECTION).  Here, KIND is a Hugo page
+kind, and Section is a Hugo section.  Both are strings.  When RESTRICT
+is supplied, only content with matching KIND and SECTION values is
+returned.  Setting either of the two to nil, or an empty string acts as
+a wildcard matching all values of the respective attribute.
+
+This function calls `hugo list' to obtain a list of site content, and
+parses the Hugo output into a grouped list containing `post-desc'
+structures, which is returned.  It is a list of the form:
+\((drafts (POST ...)) (future (POST ...))
+ (published (POST ...)) (expired (POST ...)))
+The car of each element is a symbol representing one of the possible
+publication states of a Hugo content element.  The cdr is a list of
+`post-desc' structs, or nil when there are no matching elements with the
+respective publication state."
+  (let ((selector-list '(drafts future published expired))
+        (filter-kind (or (car restrict) ""))
+        (filter-section (or (cdr restrict) ""))
+        result)
+    (dolist (sel selector-list)
+      (let (bol eol members)
+        (with-temp-buffer
+          (call-process hugoista-hugo-command nil t nil
+                        ;; command line args
+                        "list" (symbol-name sel))
+          (goto-char (point-min))
+          ;; skip header line
+          (forward-line 1)
+          ;; parse all subsequent lines
+          (setq bol (pos-bol)
+                eol (pos-eol))
+          (while (not (= bol eol))
+            (seq-let (path slug title date expiryDate publishDate draft
+                           permalink kind section)
+                (parse-csv->list (buffer-substring-no-properties bol eol))
+              ;; Drafts with a future release date appear in both,
+              ;; `hugo list drafts', and `hugo list future'. To avoid
+              ;; double appearance in the hugoista buffer, we file
+              ;; such entries under drafts only.
+              (if (or (not (equal sel 'future))
+                      (not (string= draft "true")))
+                  ;; filter entries based on criteria in RESTRICT;
+                  ;; empty value matches all
+                  (when (or (and (string-empty-p filter-kind)
+                                 (string-empty-p filter-section))
+                            (and (string= filter-kind kind)
+                                 (string= filter-section section))
+                            (and (not (string-empty-p filter-kind))
+                                 (string-empty-p filter-section)
+                                 (string= filter-kind kind))
+                            (and (string-empty-p filter-kind)
+                                 (not (string-empty-p filter-section))
+                                 (string= filter-section section)))
+                    (setq members (push (make-post-desc :path path
+                                                        :slug slug
+                                                        :title title
+                                                        :date date
+                                                        :expiryDate expiryDate
+                                                        :publishDate publishDate
+                                                        :draft draft
+                                                        :permalink permalink
+                                                        :kind kind
+                                                        :section section)
+                                        members)))))
+            (forward-line 1)
+            (setq bol (pos-bol)
+                  eol (pos-eol)))
+          (setq members (nreverse members)
+                result (push (list sel members) result)))))
+    (nreverse result)))
+
+(defun hugoista--list-posts ()
+  "Obtain a list of post pages from Hugo."
+  (hugoista--list-content (cons "page" hugoista-posts-dir)))
+
+(defun hugoista--tabulated-list-groups (content-groups)
+  "Produce a structure suitable to set to the variable `tabulated-list-groups'.
+
+CONTENT-GROUPS is a grouped list in the format produced by
+`hugoista--list-content'."
+  (mapcar (lambda (group)
+            (append (list (upcase (symbol-name (car group))))
+                    (mapcar (lambda (member)
+                              (list member
+                                    (vector (substring-no-properties
+                                             (post-desc-date member) 0 10)
+                                            (if (string= (post-desc-date member)
+                                                         (post-desc-publishDate member))
+                                                ""
+                                              (substring-no-properties
+                                               (post-desc-publishDate member) 0 10))
+                                            (if (string= "0001-01-01T00:00:00Z"
+                                                         (post-desc-expiryDate member))
+                                                ""
+                                              (substring-no-properties
+                                               (post-desc-expiryDate member) 0 10))
+                                            (post-desc-title member))))
+                            (cadr group))))
+          content-groups))
+
+(defun hugoista--content-dir ()
+  "Query Hugo for the top-level content directory of the site."
+  (with-temp-buffer
+    (call-process hugoista-hugo-command nil t nil
+                  ;; command line args
+                  "config")
+    (goto-char (point-min))
+    (when (re-search-forward "^contentdir = '\\([^']+\\)'$" nil t)
+      (match-string-no-properties 1))))
+
+(defun hugoista--hugo-site-dir-p (dir)
+  "Test whether DIR is a Hugo site directory."
+  (or (file-exists-p (expand-file-name "hugo.toml" dir))
+      (file-exists-p (expand-file-name "config.toml" dir))))
+
+;;;; Interactive Functions
+
+(defun hugoista-reload (&optional _ignore-auto _noconfirm)
+  "Refresh the contents of a `hugoista-mode' buffer."
+  (interactive)
+  (setq tabulated-list-groups
+        (hugoista--tabulated-list-groups
+         (hugoista--list-posts)))
+  (tabulated-list-print t t))
+
+(defun hugoista-visit-post ()
+  "Visit the file containing the Hugo post in the current row."
+  (interactive)
+  (find-file (expand-file-name (post-desc-path (tabulated-list-get-id))
+                                 hugoista-site-dir)))
+
+(defun hugoista-new-post ()
+  "Create a new Hugo post file, querying for the file name."
+  (interactive)
+  (let* ((content-dir (expand-file-name (hugoista--content-dir)
+                                        hugoista-site-dir))
+         (posts-dir (file-name-as-directory (expand-file-name
+                                             hugoista-posts-dir
+                                             content-dir)))
+         (new-file (read-file-name "New post file " posts-dir))
+         (new-post (file-relative-name new-file content-dir)))
+    (with-temp-buffer
+      (call-process hugoista-hugo-command nil t nil
+                    ;; command line args
+                    "new" "content" new-post))
+    (hugoista-reload)))
+
+;;;; Tabulated List Derived Major Mode, and Entry Point Listing Function
+
+(defun hugoista--date-less-p (e1 e2)
+  "Sorting predicate for the date column in `hugoista-mode'.
+
+E1 and E2 are `post-desc' structs."
+  (let ((t1 (date-to-time (post-desc-date (car e1))))
+        (t2 (date-to-time (post-desc-date (car e2)))))
+    (time-less-p t1 t2)))
+
+(defun hugoista--publishDate-less-p (e1 e2)
+  "Sorting predicate for the publication date column in `hugoista-mode'.
+
+E1 and E2 are `post-desc' structs."
+  (let ((t1 (date-to-time (post-desc-publishDate (car e1))))
+        (t2 (date-to-time (post-desc-publishDate (car e2)))))
+    (time-less-p t1 t2)))
+
+(defun hugoista--expiryDate-less-p (e1 e2)
+  "Sorting predicate for the expiration date column in `hugoista-mode'.
+
+E1 and E2 are `post-desc' structs."
+  (let ((t1 (date-to-time (post-desc-expiryDate (car e1))))
+        (t2 (date-to-time (post-desc-expiryDate (car e2)))))
+    (time-less-p t1 t2)))
+
+;;;###autoload
+(defvar-keymap hugoista-mode-map
+  :doc "Keymap for buffers created by `hugoista'."
+  :parent tabulated-list-mode-map
+  "RET" #'hugoista-visit-post
+  "N"   #'hugoista-new-post)
+
+;;;###autoload
+(define-derived-mode hugoista-mode tabulated-list-mode "Hugoista"
+  "Major mode for buffers made by \\[hugoista]."
+  :interactive nil
+  (read-only-mode 1)
+  (setq tabulated-list-format [("Posted"   12 hugoista--date-less-p)
+                               ("Releases" 12 hugoista--publishDate-less-p)
+                               ("Expires"  12 hugoista--expiryDate-less-p)
+                               ("Title"     0 t)])
+  (setq tabulated-list-padding 3)
+  (setq tabulated-list-sort-key '("Posted" . hugoista-initial-sort))
+  (tabulated-list-init-header)
+  (make-local-variable 'hugoista-site-dir)
+  (make-local-variable 'hugoista-posts-dir))
+
+;;;###autoload
+(defun hugoista (&optional dir)
+  "Manage posts on a Hugo-powered site like a barista.
+
+Presents all posts of the Hugo site rooted at DIR (defaulting to
+`hugoista-site-dir') in a tabular overview, categorised by post status.
+Posts can be acted upon using the following keys:
+
+\\<hugoista-mode-map>
+- \\[hugoista-visit-post] visits the file containing the post
+
+- \\[hugoista-new-post] creates a new post, querying for a file name
+
+\\<tabulated-list-mode-map>
+The list buffer uses major mode `hugoista-mode', which is derived from
+`tabulated-list-mode'.  Thus, all the usual key bindings for tabulated
+lists are available in `hugoista-mode-map', such as for instance
+\\[revert-buffer] for updating the table contents after one or more of the files have
+been modified."
+  (interactive)
+  (let ((buffer-dir (file-name-as-directory (or dir hugoista-site-dir))))
+    (if (not (hugoista--hugo-site-dir-p buffer-dir))
+        (message "No Hugo site found at \"%s\"" buffer-dir)
+      (if (not (executable-find hugoista-hugo-command))
+          (message "Hugo executable \"%s\" not found" hugoista-hugo-command)
+        (let ((buf (get-buffer-create
+                    (format "*%s*" (abbreviate-file-name buffer-dir)))))
+          (switch-to-buffer buf)
+          (hugoista-mode)
+          (setq hugoista-site-dir buffer-dir
+                default-directory buffer-dir)
+          (setq-local revert-buffer-function #'hugoista-reload)
+          (revert-buffer))))))
+
+(provide 'hugoista)
+;;; hugoista.el ends here

fd24d38e253cf4b59ad8cf35702659a0178cdb27
Jose E. Marchesi <jose.marchesi@oracle.com>
2025-04-22 02:56:12 +0200

    Remove add-hook to now nonexistent a68--after-change-function

--- a68-mode.el
+++ a68-mode.el
@@ -542,7 +542,6 @@
                 :backward-token #'a68--smie-backward-token)
     (setq-local beginning-of-defun-function #'a68-beginning-of-defun-upper)
     (setq-local syntax-propertize-function #'a68-syntax-propertize-function-upper))
-  (add-hook 'after-change-functions #'a68--after-change-function nil t)
   (add-hook 'syntax-propertize-extend-region-functions
             #'syntax-propertize-multiline 'append 'local)
   (setq-local comment-start a68-comment-style)

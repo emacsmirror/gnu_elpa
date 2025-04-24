583951606e4e19fd53acd8c5691210b9acb524d7
Stefan Monnier <monnier@iro.umontreal.ca>
2025-04-24 17:14:36 -0400

    * resources/make-nonascii.el: Fix header

--- resources/make-nonascii.el
+++ resources/make-nonascii.el
@@ -1,5 +1,28 @@
 ;;; make-nonascii.el --- Make an ASCII file non-ASCII  -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2025  Free Software Foundation, Inc.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Just a helper to turn an ASCII file into a non-ASCII one while
+;; trying to preserve the "structure" and make it still highlighted
+;; and indented in the same way.
+
+;;; Code:
+
 (defun elb--make-nonascii ()
   "."
   (interactive)
@@ -37,3 +60,5 @@
                       (?z . "ź")))))
         (when newchar
           (replace-match (cdr newchar) t t))))))
+
+;;; make-nonascii.el ends here

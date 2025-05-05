4acc17e8e10a95f6f0b2d76b13b649f231aec21f
Christian Johansson <christian@cvj.se>
2025-05-05 10:23:51 +0200

    Updated copyright years

--- parser-generator.el
+++ parser-generator.el
@@ -1,6 +1,6 @@
 ;;; parser-generator.el --- Parser Generator library -*- lexical-binding: t -*-
 
-;; Copyright (C) 2020-2024  Free Software Foundation, Inc.
+;; Copyright (C) 2020-2025  Free Software Foundation, Inc.
 
 ;; Author: Christian Johansson <christian@cvj.se>
 ;; Maintainer: Christian Johansson <christian@cvj.se>
--- test/parser-generator-lr-test.el
+++ test/parser-generator-lr-test.el
@@ -1,6 +1,6 @@
 ;; parser-generator-lr-test.el --- Tests for LR(k) Parser Generator -*- lexical-binding: t -*-
 
-;; Copyright (C) 2020-2024  Free Software Foundation, Inc.
+;; Copyright (C) 2020-2025  Free Software Foundation, Inc.
 
 
 ;;; Commentary:

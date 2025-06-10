2ce7b7575283eb4cf4230de3d434484d06e9fa0c
aurtzy <aurtzy@gmail.com>
2025-06-10 16:51:30 -0400

    Limit cases where display-buffer override can apply
    Providing these numerous `display-buffer` action overrides reveals that certain
    commands which call `display-buffer` more than once during
    execution (e.g. consult commands with previews) may not play nice; for example,
    using `display-buffer-below-selected` with `consult-buffer` causes a new window
    to be spawned under the selected window every time a new buffer is previewed.
    
    With some cherry-picking from `display-buffer-override-next-command`, we limit
    the cases in which the override may be applied.
    
    However, as some limitations are now present, this changes the behavior of
    `display-buffer` such that certain displays no longer respect the override, even
    when they might actually be desired.  This is deemed acceptable, given that it
    would otherwise render certain override/command combinations unusable, and users
    may still be able to configure the behavior through other means, such as
    `display-buffer-alist`.

--- disproject.el
+++ disproject.el
@@ -1139,8 +1139,10 @@ project's root directory.
                    (disproject-scope-selected-project-ensure
                     disproject--environment-scope)))
          (from-directory (project-root project))
+         (old-display-buffer-overriding-action display-buffer-overriding-action)
          (display-buffer-action (disproject-scope-display-buffer-action
                                  disproject--environment-scope))
+         (minibuffer-depth (minibuffer-depth))
          ;; Only enable envrc if the initial environment has it enabled.
          (enable-envrc (and (bound-and-true-p envrc-mode)
                             (symbol-function 'envrc-mode)))
@@ -1156,7 +1158,22 @@ project's root directory.
             ;; If an override is used and `switch-to-buffer' is called, we
             ;; assume the user explicitly wants it to obey the display action.
             (switch-to-buffer-obey-display-actions (and display-buffer-action t))
-            (display-buffer-overriding-action display-buffer-action))
+            ;; Don't use `display-buffer-override-next-command' to avoid having
+            ;; to infer type in PRE-FUNCTION.  Also makes it possible to use
+            ;; `display-buffer-no-window', which would not have valid values to
+            ;; return.
+            (display-buffer-overriding-action
+             `(,(lambda (buffer alist)
+                  (unless (> (minibuffer-depth) minibuffer-depth)
+                    (pcase display-buffer-action
+                      (`(,functions . ,action-alist)
+                       (prog1
+                           (seq-some (lambda (fun)
+                                       (funcall fun buffer
+                                                (append action-alist alist)))
+                                     (ensure-list functions))
+                         (setq display-buffer-overriding-action
+                               old-display-buffer-overriding-action)))))))))
         (disproject-with-root
           (hack-dir-local-variables-non-file-buffer)
           ;; Make sure commands are run in the correct direnv environment if

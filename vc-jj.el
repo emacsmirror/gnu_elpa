bae49911b93093970788c29f09d532a7b67df88d
Kristoffer Balintona <krisbalintona@gmail.com>
2025-06-17 02:40:26 -0500

    Support deleting bookmarks in `vc-jj-log-view-mode` buffers
    Define `vc-jj-bookmark-delete` and bind it in
    `vc-jj-log-view-mode-map`.  This command delete the bookmark of the
    revision at point, prompting for multiple bookmarks to delete if there
    are multiple bookmarks on that revision.

--- vc-jj.el
+++ vc-jj.el
@@ -581,7 +581,8 @@ If REV is not specified, revert the file as with `vc-jj-revert'."
   (keymap-set vc-jj-log-view-mode-map "r" #'vc-jj-edit-change)
   (keymap-set vc-jj-log-view-mode-map "x" #'vc-jj-abandon-change)
   (keymap-set vc-jj-log-view-mode-map "i" #'vc-jj-new-change)
-  (keymap-set vc-jj-log-view-mode-map "b s" #'vc-jj-bookmark-set))
+  (keymap-set vc-jj-log-view-mode-map "b s" #'vc-jj-bookmark-set)
+  (keymap-set vc-jj-log-view-mode-map "b D" #'vc-jj-bookmark-delete))
 
 
 (defun vc-jj-expanded-log-entry (revision)
@@ -693,6 +694,26 @@ user first."
           (user-error "Aborted moving bookmark %s to revision %s" bookmark target-rev)))
       (vc-jj--command-dispatched nil 0 nil "bookmark" "set" bookmark "-r" target-rev
                                  "--allow-backwards" "--quiet")
+      (revert-buffer))))
+
+(defun vc-jj-bookmark-delete ()
+  "Delete bookmark of the revision at point.
+When called in a `vc-jj-log-view-mode' buffer, delete the bookmark of
+the revision at point.  If there are multiple bookmarks attached to the
+revision, prompt the user to choose one or more of these bookmarks to
+delete."
+  (interactive)
+  (when (derived-mode-p 'vc-jj-log-view-mode)
+    (let* ((rev (log-view-current-tag))
+           (revision-bookmarks
+            (string-split (vc-jj--command-parseable "show" "-r" rev "--no-patch"
+                                                    "-T" "self.local_bookmarks() ++ \"\n\"")
+                          " " t "\n"))
+           (bookmarks
+            (if (< 1 (length revision-bookmarks))
+                (completing-read-multiple "Delete bookmarks: " revision-bookmarks nil t)
+              revision-bookmarks)))
+      (apply #'vc-jj--command-dispatched nil 0 nil "--quiet" "bookmark" "delete" bookmarks)
       (revert-buffer))))
 
 (defun vc-jj-root (file)

035dc4839976f4b59b1a3c5e85d61bf6abc04558
Lucas Quintana <lmq10@protonmail.com>
2025-07-28 23:12:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# iso-date: A simple library for working with ISO (YYYY-MM-DD) dates in Emacs
+
+ISO dates (such as `2025-07-28`), being as they are a well-known standard in software, have the potential to connect your computing environment. This library aims to provide utilities that makes working with ISO dates in Emacs simple and fun, both for users and developers.
+
+## For users
+
+This library offers some commands which let you use ISO dates with several Emacs packages:
+
+- iso-date-show-calendar
+- iso-date-show-org-agenda
+- iso-date-show-diary
+- iso-date-send-to-calc
+
+They are particularly useful as `embark` actions (see [this pull request](https://github.com/oantolin/embark/pull/769) for context).
+
+There are also commands to insert or manipulate ISO dates:
+
+- iso-date-insert
+- iso-date-at-point-day-up
+- iso-date-at-point-day-down
+- iso-date-at-point-do-shift
+
+## For developers
+
+This library provides the `iso-date` function. Without arguments, it returns an ISO date string for current day:
+
+```elisp
+(iso-date)
+=> e.g. "2025-07-28"
+```
+
+It can also take keywords which modify the date returned.
+
+```elisp
+(iso-date :day +3)
+=> e.g. "2025-07-31"
+```
+
+Valid keywords are :day, :month and :year. This does not simply manipulates the string; it parses the date, uses `decoded-time-add`, and then reconverts it to ISO. So you never end up with an invalid date.
+
+`iso-date` can also take the :start-day keyword, which changes the starting point relative to which modifications are applied.
+
+```elisp
+(iso-date :start-date "2000-12-18" :day +7)
+=> "2000-12-25"
+```
+
+Apart from this, the library provides other functions which may be useful for packages dealing with ISO dates.
+
+For converting from/back to ISO:
+
+- iso-date-to-calendar
+- iso-date-to-calendar-lax
+- iso-date-from-calendar
+- iso-date-to-internal
+- iso-date-from-internal
+
+For validating dates:
+
+- iso-date-valid-p
+
+For extracting values:
+
+- iso-date-year
+- iso-date-month
+- iso-date-day
+
+Others:
+
+- iso-date-between-dates-p
+
+Last but not least, this library can add `date` as a thing recognized by the `thingatpt` library. Just add the following to your init file:
+
+```elisp
+(iso-date-configure-thingatpt)
+```
+
+The `iso-date-at-point` and `iso-date-bounds` functions are already provided. They configure `thingatpt` when first used.
+
+------
+
+**NOTE**: `iso-date` is a high-level library. If you just need to parse an ISO date, use the built-in `iso8601` library, or its wrapper `time-date`.
---
+++ iso-date.el
@@ -0,0 +1,305 @@
+;;; iso-date.el -- Utilities for working with ISO dates -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025  Lucas Quintana
+
+;; Author: Lucas Quintana <lmq10@protonmail.com>
+;; Maintainer: Lucas Quintana <lmq10@protonmail.com>
+;; URL: https://github.com/lmq-10/iso-date
+;; Created: 2025-07-28
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "28.1"))
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This simple library provides utilities for working with ISO date
+;; strings (formatted as YYYY-MM-DD).  This includes:
+;;
+;; - Returning an ISO date in a relative way (e.g. two days from now)
+;;
+;; - Converting to Emacs internal format
+;;
+;; - Converting to `calendar.el' internal format
+;;
+;; - Integrating ISO dates with `thingatpt.el'
+;;
+;; - User commands for manipulating dates or integrating them with
+;;   external packages
+
+;;; Code:
+
+(require 'calendar)
+(require 'thingatpt)
+(require 'time-date)
+
+(defconst iso-date-regexp
+  (rx (group (= 4 digit)) "-"
+      (group (= 2 digit)) "-"
+      (group (= 2 digit)))
+  "Regexp matching an ISO 8601 date string.
+
+This means a date formatted as YYYY-MM-DD.")
+
+;;;; Conversion
+
+(defun iso-date-to-calendar (date)
+  "Convert ISO 8601 DATE to calendar internal format.
+
+Provided that DATE follows ISO, the return value will always be a real
+date, even if DATE is non-existant; for instance, 2025-02-31 becomes (3
+3 2025) which really means 2025-03-03.  If you need to remain faithful
+to the original string, use `iso-date-to-calendar-lax' instead.
+
+See also `iso-date-from-calendar'."
+  (when (string-match-p iso-date-regexp date)
+    (calendar-gregorian-from-absolute (date-to-day date))))
+
+(defun iso-date-to-calendar-lax (date)
+  "Convert ISO 8601 DATE to calendar internal format.
+
+This function does not do internal conversions, so the resulting date
+can be non-existant.  It will, however, be faithful to what DATE really
+shows."
+  (when (string-match iso-date-regexp date)
+    (let ((day (match-string 3 date))
+          (month (match-string 2 date))
+          (year (match-string 1 date)))
+      (mapcar #'string-to-number (list month day year)))))
+
+(defun iso-date-from-calendar (date)
+  "Convert DATE from calendar internal format to ISO 8601 format.
+
+See also `iso-date-to-calendar'."
+  (when (and (listp date) (length= date 3))
+    (format-time-string
+     "%F"
+     (days-to-time (- (calendar-absolute-from-gregorian date) (time-to-days 0))))))
+
+(defun iso-date-to-internal (date)
+  "Convert an ISO 8601 DATE string to an Emacs timestamp.
+The timestamp points to time 00:00 at that day.
+
+See also `iso-date-from-internal'."
+  (when (string-match-p iso-date-regexp date)
+    (date-to-time date)))
+
+(defun iso-date-from-internal (ts)
+  "Convert TS to an ISO 8601 date string.
+See `format-time-string' for the values TS can take.
+
+See also `iso-date-to-internal'."
+  (format-time-string "%F" ts))
+
+;;;; Creation of strings
+
+(defun iso-date (&rest keywords)
+  "Return an ISO 8601 date string for current day.
+
+KEYWORDS allow to modify the date returned.  They are passed to
+`make-decoded-time'.  For instance, the following returns a date string
+for yesterday:
+
+\(iso-date :day -1)
+
+A special keyword named START-DATE allows to set the starting day which
+will be modified by the rest of KEYWORDS.  It should be an ISO 8601 date
+string.  For instance, to add a month to a specific date:
+
+\(iso-date :start-date \"2000-12-18\" :month +1)"
+  (format-time-string
+   "%F"
+   (when keywords
+     (encode-time
+      (decoded-time-add
+       (if-let* ((date (plist-get keywords :start-date)))
+           (progn
+             (setq keywords (remove :start-date (remove date keywords)))
+             (parse-time-string date))
+         (decode-time))
+       (apply #'make-decoded-time keywords))))))
+
+(defun iso-date-shift (shift date)
+  "Apply SHIFT to DATE, return result.
+
+SHIFT should be a string representing a time shift such as +4w.
+Supported letters are d (day), w (week), m (month) and y (year).
+
+DATE should be an ISO 8601 date string.  Returned date is also in
+that format.
+
+This is an alternative to the plist-based modification offered by
+`iso-date'."
+  (when (string-match "\\`[ \t]*\\([+-]?[0-9]+\\)\\([a-z]\\)[ \t]*\\'" shift)
+    (let ((num (string-to-number (match-string 1 shift)))
+          (unit (match-string 2 shift)))
+      (iso-date
+       :start-date date
+       (pcase unit
+         ("d" :day)
+         ("w" (and (setq num (* 7 num)) :day))
+         ("m" :month)
+         ("y" :year)
+         (_ (error "Unsupported time unit")))
+       num))))
+
+;;;; Extraction
+
+(defun iso-date-year (date)
+  "Return year component from ISO DATE, as an integer."
+  (when (string-match iso-date-regexp date)
+    (string-to-number (match-string 1 date))))
+
+(defun iso-date-month (date)
+  "Return month component from ISO DATE, as an integer."
+  (when (string-match iso-date-regexp date)
+    (string-to-number (match-string 2 date))))
+
+(defun iso-date-day (date)
+  "Return day component from ISO DATE, as an integer."
+  (when (string-match iso-date-regexp date)
+    (string-to-number (match-string 3 date))))
+
+;;;; Validation
+
+(defun iso-date-valid-p (date)
+  "Return non-nil if DATE is a valid ISO date."
+  (when (string-match-p iso-date-regexp date)
+    (calendar-date-is-valid-p (iso-date-to-calendar-lax date))))
+
+;;;; Basic integration with other packages
+
+(defun iso-date--read ()
+  "Read date in YYYY-MM-DD format."
+  (let ((date (read-string "Date (YYYY-MM-DD): ")))
+    (unless (string-match-p iso-date-regexp date)
+      (user-error "Date not in YYYY-MM-DD format"))
+    date))
+
+(defun iso-date-show-calendar (date)
+  "Display calendar and go to DATE."
+  (interactive (list (iso-date--read)))
+  (require 'calendar)
+  (calendar)
+  (calendar-goto-date (iso-date-to-calendar date)))
+
+(defun iso-date-show-org-agenda (date)
+  "Show Org agenda for DATE."
+  (interactive (list (iso-date--read)))
+  (require 'org-agenda)
+  (org-agenda-list nil date))
+
+(declare-function diary-check-diary-file "diary-lib")
+
+(defun iso-date-show-diary (date)
+  "Display a buffer with diary entries for DATE."
+  (interactive (list (iso-date--read)))
+  (require 'diary-lib)
+  (diary-check-diary-file)
+  (diary-list-entries (iso-date-to-calendar date) 1))
+
+(declare-function calc-push-list "calc")
+
+(defun iso-date-send-to-calc (date)
+  "Insert DATE into a calc window."
+  (interactive (list (iso-date--read)))
+  (require 'calc)
+  (require 'calendar)
+  (let ((abs (calendar-absolute-from-gregorian (iso-date-to-calendar date))))
+    (calc)
+    (calc-push-list `((date ,abs)))))
+
+(declare-function org-read-date "org")
+
+(defun iso-date-insert (&optional arg)
+  "Insert an ISO date at point.
+
+When ARG is non-nil (interactively, with a prefix argument), prompt for
+a date using `org-read-date'."
+  (interactive "P")
+  (if (not arg)
+      (insert (format-time-string "%F"))
+    (require 'org)
+    (insert (org-read-date))))
+
+(defun iso-date-at-point-day-up (&optional n)
+  "Advance ISO date at point by one day.
+
+With a prefix argument N, advance by N days."
+  (interactive "p")
+  (when-let* ((bounds (iso-date-bounds))
+              (date (iso-date-at-point)))
+    (save-excursion
+      (delete-region (car bounds) (cdr bounds))
+      (insert (iso-date :start-date date :day n)))))
+
+(defun iso-date-at-point-day-down (&optional n)
+  "Advance ISO date at point by one day.
+
+With a prefix argument N, advance by (- N) days."
+  (interactive "p")
+  (iso-date-at-point-day-up (- n)))
+
+(defun iso-date-at-point-do-shift (shift)
+  "Advance ISO date at point by SHIFT.
+
+See `iso-date-shift' for the values SHIFT can take."
+  (interactive "sShift (e.g. +3d): ")
+  (when-let* ((bounds (iso-date-bounds))
+              (date (iso-date-at-point)))
+    (save-excursion
+      (delete-region (car bounds) (cdr bounds))
+      (insert (iso-date-shift shift date)))))
+
+;;;; Misc.
+
+(defun iso-date-between-dates-p (start end date)
+  "Return non-nil if DATE (an ISO string) is somewhere between START and END."
+  (or
+   (and (string-collate-lessp start date) (string-collate-lessp date end))
+   (equal start date)
+   (equal end date)))
+
+;;;; thingatpt.el integration
+
+(defun iso-date-configure-thingatpt ()
+  "Configure `date' as a valid thing understood by `thing-at-point'."
+  (put 'date 'bounds-of-thing-at-point
+       (lambda ()
+         (let ((thing (thing-at-point-looking-at iso-date-regexp 10)))
+           (when thing
+             (cons (match-beginning 0) (match-end 0))))))
+  (put 'date 'thing-at-point
+       (lambda ()
+         (let ((boundary-pair (bounds-of-thing-at-point 'date)))
+           (when boundary-pair
+             (buffer-substring-no-properties
+              (car boundary-pair) (cdr boundary-pair)))))))
+
+(defun iso-date-at-point ()
+  "Return ISO date at point."
+  (unless (get 'date 'thing-at-point)
+    (iso-date-configure-thingatpt))
+  (thing-at-point 'date t))
+
+(defun iso-date-bounds ()
+  "Return bounds of ISO date at point."
+  (unless (get 'date 'thing-at-point)
+    (iso-date-configure-thingatpt))
+  (bounds-of-thing-at-point 'date))
+
+(provide 'iso-date)
+;;; iso-date.el ends here

de25aad37999c14fa232b8f0b9cac8954037a1df
Alvin Hsu <aurtzy@gmail.com>
2025-10-03 02:28:57 -0400

    Bump version to 2.2.0

--- disproject.el
+++ disproject.el
@@ -8,7 +8,7 @@
 ;; URL: https://github.com/aurtzy/disproject
 ;; Keywords: convenience, files, vc
 
-;; Version: 2.1.0
+;; Version: 2.2.0
 ;; Package-Requires: ((emacs "29.4") (transient "0.9.2"))
 
 ;; This program is free software; you can redistribute it and/or modify

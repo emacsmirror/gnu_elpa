b5d8fad4ac9481589294a2c4c2d392c33eb5bae2
Michael Albinus <michael.albinus@gmx.de>
2025-11-09 14:38:12 +0100

    * debbugs.el: Bump version to 0.46.

--- debbugs.el
+++ debbugs.el
@@ -5,7 +5,7 @@
 ;; Author: Michael Albinus <michael.albinus@gmx.de>
 ;; Keywords: comm, hypermedia
 ;; Package: debbugs
-;; Version: 0.45
+;; Version: 0.46
 ;; Package-Requires: ((emacs "26.1") (soap-client "3.1.5"))
 
 ;; This file is not part of GNU Emacs.

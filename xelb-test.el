7b63460003ee8f01e70fd2aed5d180d6f1fb3bc7
Daniel Mendler <mail@daniel-mendler.de>
2026-01-01 14:35:01 +0100

    ; Update copyright years

--- xelb-test.el
+++ xelb-test.el
@@ -1,6 +1,6 @@
 ;;; xelb-test.el --- Unit tests for XELB -*- lexical-binding: t -*-
 
-;; Copyright (C) 2024-2025 Free Software Foundation, Inc.
+;; Copyright (C) 2024-2026 Free Software Foundation, Inc.
 
 ;; Author: Steven Allen <steven@stebalien.com>
 

021b31f791b2a0934cf5931a7f067b983a79e0f6
Protesilaos Stavrou <info@protesilaos.com>
2026-01-11 11:34:58 +0200

    Update copyright years and fix the starting year

--- denote-silo.el
+++ denote-silo.el
@@ -1,6 +1,6 @@
 ;;; denote-silo.el --- Convenience functions for using Denote in multiple silos  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023-2025  Free Software Foundation, Inc.
+;; Copyright (C) 2023-2026  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>

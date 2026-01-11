022ee16dbe18b5fe5b9a18872638ee6c2c55e91e
Protesilaos Stavrou <info@protesilaos.com>
2026-01-11 11:32:56 +0200

    Update copyright years and fix the starting year

--- denote-sequence.el
+++ denote-sequence.el
@@ -1,6 +1,6 @@
 ;;; denote-sequence.el --- Sequence notes or Folgezettel with Denote -*- lexical-binding: t -*-
 
-;; Copyright (C) 2024-2025  Free Software Foundation, Inc.
+;; Copyright (C) 2024-2026  Free Software Foundation, Inc.
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>

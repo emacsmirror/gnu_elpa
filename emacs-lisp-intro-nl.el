20f170a1183fde44905ced86dc34a0718811765e
matto <matto@matto.nl>
2026-01-29 17:45:10 +0100

    prepare for elpa.gnu.org

---
+++ emacs-lisp-intro-nl.el
@@ -0,0 +1,15 @@
+;;; emacs-lisp-intro-nl.el --- ELisp intro manual in NL  -*- lexical-binding:t -*-
+
+;; Copyright (C) 2026  Free Software Foundation, Inc.
+;;
+;; Author: Matto Fransen <matto@matto.nl>
+;; Version: 0
+
+;;; Commentary:
+
+;; This is just a dummy file that holds the package's metadata.
+
+;;; Code:
+
+(provide 'emacs-lisp-intro-nl)
+;;; emacs-lisp-intro-nl.el ends here

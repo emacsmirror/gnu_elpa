979a23dc76e313283689a819d580f81fb3ccc5a2
Protesilaos Stavrou <info@protesilaos.com>
2026-02-11 00:42:48 +0200

    Add xref--xref-buffer-mode-hook to the lin-mode-hooks
    That major-mode should not have a double dash, but I know it is okay
    because I have been relying on it for years in the Denote package.

--- lin.el
+++ lin.el
@@ -92,7 +92,8 @@
     proced-mode-hook
     tabulated-list-mode-hook
     tar-mode-hook
-    world-clock-mode-hook)
+    world-clock-mode-hook
+    xref--xref-buffer-mode-hook)
   "List of hooks that should be used by the `lin-global-mode'.
 Lin activates `hl-line-mode' and remaps its face to `lin-face'.
 This makes it possible to tweak the `lin-face' in order to

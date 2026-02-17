2cfea6b297a0c1eab6bece50d350555e9795afa0
Stefan Monnier <monnier@iro.umontreal.ca>
2026-02-17 15:47:30 -0500

    Prepare for GNU ELPA
    * emacs-lisp-intro-es.el: New file, need for ELPA.
    * History: Mention the specific revision of the original text.

---
+++ emacs-lisp-intro-es.el
@@ -0,0 +1,15 @@
+;;; emacs-lisp-intro-es.el --- ELisp intro manual in Spanish  -*- lexical-binding:t -*-
+
+;; Copyright (C) 2026  Free Software Foundation, Inc.
+;;
+;; Author: David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
+;; Version: 0
+
+;;; Commentary:
+
+;; This is just a dummy file that holds the package's metadata.
+
+;;; Code:
+
+(provide 'emacs-lisp-intro-es)
+;;; emacs-lisp-intro-es.el ends here

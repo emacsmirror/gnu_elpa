ef7714b2528b7c74034a1568a753a56dcc8ca877
Jonas Bernoulli <jonas@bernoul.li>
2026-02-26 13:56:39 +0100

    Document contact options in library commentary
    I used to believe that it was enough that the library header
    contains
    
      ;; Author: Jonas Bernoulli <emacs.transient@jonas.bernoulli.dev>
      ;; Homepage: https://github.com/magit/transient
    
    and https://elpa.gnu.org/packages/transient.html contains
    
      Maintainer
      Jonas Bernoulli <emacs.transient@jonas.bernoulli.dev>
      Website
      https://github.com/magit/transient
    
    and the output of M-x describe-package RET transient RET contains
    
        Website: https://github.com/magit/transient
     Maintainer: Jonas Bernoulli <emacs.transient@jonas.bernoulli.dev>
         Author: Jonas Bernoulli <emacs.transient@jonas.bernoulli.dev>
    
    to imply that either one or the other can be used to contact me,
    the maintainer, about Transient; but the consensus on emacs-devel
    seems to be that this information has to be spelled out in the
    library commentary, so that's what I do now.
    
    (Also note that the email address contains the substring "transient",
    which implies that it is intended for inquiries about Transient.)

--- lisp/transient.el
+++ lisp/transient.el
@@ -36,6 +36,12 @@
 ;; in Magit.  It is distributed as a separate package, so that it can be
 ;; used to implement similar menus in other packages.
 
+;; You can contact the maintainer of this package by sending an email to
+;; <emacs.transient@jonas.bernoulli.dev>, or you can use the public issue
+;; tracker at <https://github.com/magit/transient>.  The latter can also
+;; be accessed using the `forge' package, which lets you avoid the sadly
+;; non-free javascript on that website.
+
 ;;; Code:
 
 (defconst transient-version "0.12.0")

6ada185f54bf94dea6820cc93d12e3a3a80ad11c
Protesilaos Stavrou <info@protesilaos.com>
2026-04-04 16:59:27 +0300

    Fix another inaccuracy in denote-sequence-scheme docstring
    Thanks again to liyingzhi for bringing this matter to my attention in
    issue 18: <https://github.com/protesilaos/denote-sequence/issues/18>.

--- denote-sequence.el
+++ denote-sequence.el
@@ -86,7 +86,7 @@ is derived via the alternation from numbers to latin characters, such
 that 1a2 refers to the second child of the first child of parent 1.
 Because they alternate between numbers and letters, they do not use the
 equals sign.  When a number cannot be represented by a single letter,
-two or more are used instead, such as the number 51 corresponding to
+two or more are used instead, such as the number 50 corresponding to
 zx (z is 26 and x is 24).
 
 Alphanumeric delimited sequences combine elements of the aforementioned.

ddc475a13dee2014bba1575907e07b6f646538c1
Thanos Apollo <public@thanosapollo.org>
2026-04-22 22:37:07 +0300

    utils: Add metadata management, editing, review, label creation

--- forgejo-utils.el
+++ forgejo-utils.el
@@ -26,6 +26,8 @@
 
 (defvar forgejo-host)
 
+(require 'forgejo-api)
+
 (declare-function forgejo-token "forgejo.el" ())
 
 ;;; URL builders
@@ -58,9 +60,6 @@
 
 ;;; State toggle
 
-(declare-function forgejo-api-patch "forgejo-api.el"
-                  (endpoint &optional json-body callback))
-
 (defun forgejo-utils-toggle-state (owner repo number current-state callback)
   "Toggle issue/PR NUMBER in OWNER/REPO between open and closed.
 CURRENT-STATE is \"open\" or \"closed\".  CALLBACK is called on success."
@@ -76,9 +75,6 @@ CURRENT-STATE is \"open\" or \"closed\".  CALLBACK is called on success."
 
 ;;; Comment
 
-(declare-function forgejo-api-post "forgejo-api.el"
-                  (endpoint &optional params json-body callback))
-
 (defun forgejo-utils-comment (owner repo number)
   "Post a comment on issue/PR NUMBER in OWNER/REPO.
 Opens a buffer for composing the comment body."
@@ -92,9 +88,6 @@ Opens a buffer for composing the comment body."
          (message "Comment posted on %s/%s#%d" owner repo number))))))
 
 ;;; Issue creation
-
-(declare-function forgejo-api-get "forgejo-api.el"
-                  (endpoint &optional params callback))
 
 (defun forgejo-utils-get-issue-templates (owner repo)
   "Fetch issue templates for OWNER/REPO synchronously.
@@ -144,6 +137,228 @@ Fetches templates if available, lets user pick one, then compose."
              `((body . ,body))))
        (lambda (_data _headers)
          (message "Issue created: %s/%s \"%s\"" owner repo title))))))
+
+;;; Label creation
+
+(declare-function forgejo-db-save-labels "forgejo-db.el"
+                  (host owner repo labels))
+
+(defun forgejo-utils-create-label (owner repo host callback)
+  "Create a new label in OWNER/REPO.
+HOST is the hostname for DB cache update.  CALLBACK is called on success."
+  (let* ((name (read-string "Label name: "))
+         (color (read-color "Label color: "))
+         (description (read-string "Description (optional): ")))
+    (when (string-empty-p name)
+      (user-error "Label name cannot be empty"))
+    ;; Convert color name to hex if needed, strip # prefix
+    (unless (string-match-p "\\`#?[0-9a-fA-F]+\\'" color)
+      (let ((rgb (color-values color)))
+        (unless rgb
+          (user-error "Unknown color: %s" color))
+        (setq color (format "%02x%02x%02x"
+                            (/ (nth 0 rgb) 256)
+                            (/ (nth 1 rgb) 256)
+                            (/ (nth 2 rgb) 256)))))
+    (when (string-prefix-p "#" color)
+      (setq color (substring color 1)))
+    (forgejo-api-post
+     (format "repos/%s/%s/labels" owner repo)
+     nil
+     `((name . ,name)
+       (color . ,color)
+       ,@(unless (string-empty-p description)
+           `((description . ,description))))
+     (lambda (data _headers)
+       ;; Cache the new label locally
+       (forgejo-db-save-labels host owner repo (list data))
+       (message "Created label %s in %s/%s" name owner repo)
+       (when callback (funcall callback))))))
+
+;;; PR review
+
+(defun forgejo-utils-submit-review (owner repo number callback)
+  "Submit a review on PR NUMBER in OWNER/REPO.
+Prompts for the review type and an optional body.
+CALLBACK is called on success."
+  (let* ((type (completing-read "Review type: "
+                                '("approve" "request_changes" "comment")
+                                nil t))
+         (event (pcase type
+                  ("approve" "APPROVED")
+                  ("request_changes" "REQUEST_CHANGES")
+                  ("comment" "COMMENT")))
+         (body (read-string-from-buffer "Review body" "")))
+    (forgejo-api-post
+     (format "repos/%s/%s/pulls/%d/reviews" owner repo number)
+     nil
+     `((event . ,event)
+       ,@(when (and body (not (string-empty-p (string-trim body))))
+           `((body . ,body))))
+     (lambda (_data _headers)
+       (message "Review submitted: %s on %s/%s#%d" type owner repo number)
+       (when callback (funcall callback))))))
+
+;;; Edit
+
+(declare-function forgejo-db--execute "forgejo-db.el" (sql &rest args))
+(declare-function forgejo-api-render-markdown-async "forgejo-api.el"
+                  (text context callback))
+
+(defun forgejo-utils-edit-body (owner repo number current-body callback)
+  "Edit the body of issue/PR NUMBER in OWNER/REPO.
+CURRENT-BODY is pre-filled in the editor.  CALLBACK is called on success."
+  (let ((body (read-string-from-buffer "Edit body" (or current-body "")))
+        (host (url-host (url-generic-parse-url forgejo-host)))
+        (context (format "%s/%s" owner repo)))
+    (when (and body (not (string= body (or current-body ""))))
+      (forgejo-api-patch
+       (format "repos/%s/%s/issues/%d" owner repo number)
+       `((body . ,body))
+       (lambda (_data _headers)
+         ;; PATCH response does not include body_html, so render
+         ;; the markdown via the API before saving to the DB.
+         (forgejo-api-render-markdown-async
+          body context
+          (lambda (html)
+            (forgejo-db--execute
+             "UPDATE issues SET body = ?, body_html = ?
+              WHERE host = ? AND owner = ? AND repo = ? AND number = ?"
+             (list body (or html "") host owner repo number))
+            (message "Updated body of %s/%s#%d" owner repo number)
+            (when callback (funcall callback)))))))))
+
+(defun forgejo-utils-edit-comment (owner repo comment-id current-body callback)
+  "Edit comment COMMENT-ID in OWNER/REPO.
+CURRENT-BODY is pre-filled in the editor.  CALLBACK is called on success."
+  (let ((body (read-string-from-buffer "Edit comment" (or current-body "")))
+        (host (url-host (url-generic-parse-url forgejo-host)))
+        (context (format "%s/%s" owner repo)))
+    (when (and body (not (string= body (or current-body ""))))
+      (forgejo-api-patch
+       (format "repos/%s/%s/issues/comments/%d" owner repo comment-id)
+       `((body . ,body))
+       (lambda (_data _headers)
+         ;; PATCH response does not include body_html, so render
+         ;; the markdown via the API before saving to the DB.
+         (forgejo-api-render-markdown-async
+          body context
+          (lambda (html)
+            (forgejo-db--execute
+             "UPDATE timeline_events SET body = ?, body_html = ?
+              WHERE host = ? AND owner = ? AND repo = ? AND id = ?"
+             (list body (or html "") host owner repo comment-id))
+            (message "Updated comment %d in %s/%s" comment-id owner repo)
+            (when callback (funcall callback)))))))))
+
+;;; Label/assignee/milestone management
+
+(declare-function forgejo-db-get-labels "forgejo-db.el"
+                  (host owner repo))
+(declare-function forgejo-db-get-label-id "forgejo-db.el"
+                  (host owner repo name))
+(declare-function forgejo-db-get-milestones "forgejo-db.el"
+                  (host owner repo))
+(declare-function forgejo-db-get-milestone-id "forgejo-db.el"
+                  (host owner repo title))
+(declare-function forgejo-db-get-authors "forgejo-db.el"
+                  (host owner repo))
+
+(defun forgejo-utils-add-label (owner repo number host callback)
+  "Add a label to issue/PR NUMBER in OWNER/REPO.
+HOST is the hostname for DB lookups.  If the entered label does not
+exist, offers to create it first.  CALLBACK is called on success."
+  (let* ((cached (forgejo-db-get-labels host owner repo))
+         (names (mapcar (lambda (row) (nth 4 row)) cached))
+         (name (completing-read "Add label: " names nil nil))
+         (id (forgejo-db-get-label-id host owner repo name)))
+    (if id
+        (forgejo-utils--apply-label owner repo number name id callback)
+      ;; Label doesn't exist, offer to create it
+      (if (y-or-n-p (format "Label %S doesn't exist. Create it? " name))
+          (forgejo-utils-create-label
+           owner repo host
+           (lambda ()
+             (let ((new-id (forgejo-db-get-label-id host owner repo name)))
+               (if new-id
+                   (forgejo-utils--apply-label owner repo number name new-id callback)
+                 (user-error "Failed to resolve label %S after creation" name)))))
+        (user-error "Label %S not found" name)))))
+
+(defun forgejo-utils--apply-label (owner repo number name id callback)
+  "Add label NAME (with ID) to issue/PR NUMBER in OWNER/REPO.
+CALLBACK is called on success."
+  (forgejo-api-post
+   (format "repos/%s/%s/issues/%d/labels" owner repo number)
+   nil
+   `((labels . ,(vector id)))
+   (lambda (_data _headers)
+     (message "Added label %s to %s/%s#%d" name owner repo number)
+     (when callback (funcall callback)))))
+
+(defun forgejo-utils-remove-label (owner repo number current-labels host callback)
+  "Remove a label from issue/PR NUMBER in OWNER/REPO.
+CURRENT-LABELS is the list of label alists on the issue.
+HOST is the hostname for DB lookups.  CALLBACK is called on success."
+  (let* ((names (mapcar (lambda (l) (alist-get 'name l)) current-labels))
+         (name (completing-read "Remove label: " names nil t))
+         (id (forgejo-db-get-label-id host owner repo name)))
+    (unless id
+      (user-error "Label %S not found in cache" name))
+    (forgejo-api-delete
+     (format "repos/%s/%s/issues/%d/labels/%d" owner repo number id)
+     nil
+     (lambda (_data _headers)
+       (message "Removed label %s from %s/%s#%d" name owner repo number)
+       (when callback (funcall callback))))))
+
+(defun forgejo-utils-add-assignee (owner repo number current-assignees host callback)
+  "Add an assignee to issue/PR NUMBER in OWNER/REPO.
+CURRENT-ASSIGNEES is the list of current assignee alists.
+HOST is the hostname for DB lookups.  CALLBACK is called on success."
+  (let* ((authors (forgejo-db-get-authors host owner repo))
+         (login (completing-read "Add assignee: " authors nil nil))
+         (current (mapcar (lambda (a) (alist-get 'login a)) current-assignees))
+         (new-list (delete-dups (append current (list login)))))
+    (when (string-empty-p login)
+      (user-error "No assignee specified"))
+    (forgejo-api-patch
+     (format "repos/%s/%s/issues/%d" owner repo number)
+     `((assignees . ,new-list))
+     (lambda (_data _headers)
+       (message "Assigned %s to %s/%s#%d" login owner repo number)
+       (when callback (funcall callback))))))
+
+(defun forgejo-utils-remove-assignee (owner repo number current-assignees callback)
+  "Remove an assignee from issue/PR NUMBER in OWNER/REPO.
+CURRENT-ASSIGNEES is the list of assignee alists.  CALLBACK on success."
+  (let* ((logins (mapcar (lambda (a) (alist-get 'login a)) current-assignees))
+         (login (completing-read "Remove assignee: " logins nil t))
+         (new-list (remove login logins)))
+    (forgejo-api-patch
+     (format "repos/%s/%s/issues/%d" owner repo number)
+     `((assignees . ,(or (vconcat new-list) [])))
+     (lambda (_data _headers)
+       (message "Unassigned %s from %s/%s#%d" login owner repo number)
+       (when callback (funcall callback))))))
+
+(defun forgejo-utils-set-milestone (owner repo number host callback)
+  "Set or clear the milestone on issue/PR NUMBER in OWNER/REPO.
+HOST is the hostname for DB lookups.  CALLBACK is called on success."
+  (let* ((cached (forgejo-db-get-milestones host owner repo))
+         (titles (mapcar (lambda (row) (nth 4 row)) cached))
+         (title (completing-read "Milestone (empty to clear): " titles nil nil))
+         (id (if (string-empty-p title) 0
+               (or (forgejo-db-get-milestone-id host owner repo title)
+                   (user-error "Milestone %S not found in cache" title)))))
+    (forgejo-api-patch
+     (format "repos/%s/%s/issues/%d" owner repo number)
+     `((milestone . ,id))
+     (lambda (_data _headers)
+       (message "%s milestone on %s/%s#%d"
+                (if (= id 0) "Cleared" (format "Set milestone %s on" title))
+                owner repo number)
+       (when callback (funcall callback))))))
 
 ;;; Filter prompt
 

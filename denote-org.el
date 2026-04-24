2133ed6c427a780b8348740268c180c9bdd391c6
Protesilaos <info@protesilaos.com>
2026-04-24 13:26:37 +0300

    Use "Protesilaos" instead of "Protesilaos Stavrou"
    As I announced here: <https://protesilaos.com/news/2026-04-21-omitting-the-surname/>.

--- denote-org.el
+++ denote-org.el
@@ -2,8 +2,8 @@
 
 ;; Copyright (C) 2024-2026  Free Software Foundation, Inc.
 
-;; Author: Protesilaos Stavrou <info@protesilaos.com>
-;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
+;; Author: Protesilaos <info@protesilaos.com>
+;; Maintainer: Protesilaos <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/denote-org
 ;; Version: 0.2.1
 ;; Package-Requires: ((emacs "28.1") (denote "4.0.0"))

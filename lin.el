65556a218fe138516ebe557726fe6e4b8b60f539
Protesilaos <info@protesilaos.com>
2026-04-24 13:27:12 +0300

    Use "Protesilaos" instead of "Protesilaos Stavrou"
    As I announced here: <https://protesilaos.com/news/2026-04-21-omitting-the-surname/>.

--- lin.el
+++ lin.el
@@ -2,8 +2,8 @@
 
 ;; Copyright (C) 2021-2026  Free Software Foundation, Inc.
 
-;; Author: Protesilaos Stavrou <info@protesilaos.com>
-;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
+;; Author: Protesilaos <info@protesilaos.com>
+;; Maintainer: Protesilaos <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/lin
 ;; Version: 2.0.0
 ;; Package-Requires: ((emacs "27.1"))

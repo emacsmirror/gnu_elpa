d2f30624298f11edbae55c36f067bb44abae4785
Milan Glacier <dev@milanglacier.com>
2026-04-26 14:02:36 -0400

    feat(duet): change default model for openai-compatible to `deepseek-v4-flash`.

--- minuet-duet.el
+++ minuet-duet.el
@@ -322,7 +322,7 @@ export async function sendUser(user: User, overrides = {}) {
   "Provider options for duet Gemini backend.")
 
 (defvar minuet-duet-openai-compatible-options
-  `(:model "minimax/minimax-m2.7"
+  `(:model "deepseek/deepseek-v4-flash"
     :api-key "OPENROUTER_API_KEY"
     :end-point "https://openrouter.ai/api/v1/chat/completions"
     :name "Openrouter"

94b7800e1a23ee41db133a355d20e8c84db08c09
Rahguzar <rahguzar@mailbox.org>
2026-04-30 17:58:41 +0500

    Elpa version to 0.7

--- hoogle-buffer.el
+++ hoogle-buffer.el
@@ -4,10 +4,10 @@
 
 ;; Created: February 06, 2024
 ;; License: GPL-3.0-or-later
-;; Version: 0.6.0
+;; Version: 0.7.0
 ;; Keywords: docs languages
 ;; Homepage: https://codeberg.org/rahguzar/consult-hoogle
-;; Package-Requires: ((emacs "27.1"))
+;; Package-Requires: ((emacs "28.1"))
 
 ;; This file is part of GNU Emacs.
 

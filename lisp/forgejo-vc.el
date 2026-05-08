cf44b61f48547370ed13bb1f27df18cf05534fe0
Thanos Apollo <public@thanosapollo.org>
2026-05-08 18:57:55 +0300

    [fix] vc: Guard against nil remote in popup description

--- lisp/forgejo-vc.el
+++ lisp/forgejo-vc.el
@@ -582,10 +582,11 @@ and mark it as manually merged after a successful push."
 
 (keymap-popup-define forgejo-vc-map
   :description (lambda ()
-		 (let ((remote (forgejo-vc--repo-from-remote)))
-		   (format "Forgejo operations for %s/%s"
-			   (propertize (nth 1 remote) 'face 'font-lock-type-face)
-			   (propertize (nth 2 remote) 'face 'font-lock-type-face))))
+		 (if-let* ((remote (forgejo-vc--repo-from-remote)))
+		     (format "Forgejo operations for %s/%s"
+			     (propertize (nth 1 remote) 'face 'font-lock-type-face)
+			     (propertize (nth 2 remote) 'face 'font-lock-type-face))
+		   "Forgejo operations"))
   :group "View"
   "i" ((lambda ()
          (cond

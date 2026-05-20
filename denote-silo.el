342d5e7e1bd63ce742a0362911c47da1ec63e868
Protesilaos <info@protesilaos.com>
2026-05-20 15:29:13 +0300

    Update denote-silo to version 0.3.0

--- denote-silo.el
+++ denote-silo.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos <info@protesilaos.com>
 ;; Maintainer: Protesilaos <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/denote-silo
-;; Version: 0.2.0
+;; Version: 0.3.0
 ;; Package-Requires: ((emacs "28.1") (denote "4.0.0"))
 
 ;; This file is NOT part of GNU Emacs.

8a50b51559debb293554f59d68f12b003ccc8b20
Amin Bandali <bandali@gnu.org>
2026-05-20 09:52:22 -0400

    Release ffs 0.2.1

--- ffs.el
+++ ffs.el
@@ -5,7 +5,7 @@
 ;; Author: Amin Bandali <bandali@gnu.org>
 ;; Maintainer: Amin Bandali <bandali@gnu.org>
 ;; URL: https://git.kelar.org/~bandali/ffs
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience, focus, narrowing, outlines, presentation, text
 

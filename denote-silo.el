41985759d29d4a673055a0d0bd08e2ada88cffe8
Protesilaos <info@protesilaos.com>
2026-07-04 18:23:53 +0300

    Fix broken parentheses

--- denote-silo.el
+++ denote-silo.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos <info@protesilaos.com>
 ;; Maintainer: Protesilaos <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/denote-silo
-;; Version: 0.3.1
+;; Version: 0.3.2
 ;; Package-Requires: ((emacs "28.1") (denote "4.0.0"))
 
 ;; This file is NOT part of GNU Emacs.
@@ -52,7 +52,7 @@ variable `denote-directory'."
   :group 'denote-silo
   :link '(info-link "(denote) Maintain separate directories for notes")
   :type '(repeat directory)
-  :package-version '(denote-silo . "0.4.0")))
+  :package-version '(denote-silo . "0.4.0"))
 
 (defvar denote-silo-directory-history nil
   "Minibuffer history for `denote-silo-directory-prompt'.")

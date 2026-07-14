5cd20085587efa2ba96669158d4acba6db4b0fe6
Paul Nelson <ultrono@gmail.com>
2026-07-14 22:44:10 +0100

    Add directory local binding to avoid rearranging bib entries

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((LaTeX-mode . ((czm-tex-ref-rearrange-bib-entries . nil))))

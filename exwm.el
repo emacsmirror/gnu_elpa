5cd76bb639df30a8a7d0f0bbf2edeacd75be0548
Daniel Mendler <mail@daniel-mendler.de>
2026-07-18 15:07:51 +0200

    ; Fix exit prompt, remove superfluous question mark

--- exwm.el
+++ exwm.el
@@ -1234,7 +1234,8 @@ If FORCE is any other non-nil value, force killing of Emacs."
             (`break (y-or-n-p prompt))
             (x x)))
          (t
-          (yes-or-no-p (format "[EXWM] %d X window(s) will be destroyed.  %s?"
+          ;; prompt already ends with ?
+          (yes-or-no-p (format "[EXWM] %d X window(s) will be destroyed.  %s"
                                (length exwm--id-buffer-alist) prompt))))
     ;; Run `kill-emacs-hook' (`server-force-stop' excluded) before Emacs
     ;; frames are unmapped so that errors (if any) can be visible.

76dc758361378e1c863151cb7febceb4d5218f6f
Protesilaos <info@protesilaos.com>
2026-07-22 14:04:18 +0300

    Update to pulsar version 1.4.0

--- pulsar.el
+++ pulsar.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos <info@protesilaos.com>
 ;; Maintainer: Protesilaos <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/pulsar
-;; Version: 1.3.4
+;; Version: 1.4.0
 ;; Package-Requires: ((emacs "28.1"))
 ;; Keywords: convenience, pulse, highlight
 

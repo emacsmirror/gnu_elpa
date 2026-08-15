bf1da925ea2e14f6234851c8df1543489d7fe20a
Philip Kaludercic <philipk@posteo.net>
2026-08-15 15:26:25 +0200

    Rewrite the package to use Harper

--- README.md
+++ README.md
@@ -1,24 +1,78 @@
-# flymake-proselint
+A Flymake Backend of Harper
+===========================
 
-This package makes it possible to use [proselint](http://proselint.com/) with
-Emacs built-in Flymake.
+This package makes it possible to use [Harper] with Emacs built-in
+[Flymake], the syntax checker UI that this package uses to present
+local grammar analysis.  This package is a fork of [flymake-proselint]
+by Manuel Uberti.
 
-## Getting started
+[Harper]:
+	https://writewithharper.com/
+[Flymake]:
+	https://www.gnu.org/software/emacs/manual/html_node/emacs/Flymake.html
+[flymake-proselint]:
+	https://elpa.gnu.org/packages/flymake-proselint.html
 
-`flymake-proselint` is listed on [GNU
-ELPA](http://elpa.gnu.org/packages/flymake-proselint.html), so you can use `M-x
-list-packages` to install it.
+Installation
+------------
 
-Then you just need to activate it in the modes you want your prose to be checked
-with something like:
+`flymake-harper.el` is available from [GNU ELPA]. It can be installed
+by invoking
 
-``` emacs-lisp
-(add-hook 'text-mode-hook (lambda ()
-                            (flymake-mode)
-                            (flymake-proselint-setup)))
-```
+	M-x package-install RET flymake-harper RET
 
-## Status
+[GNU ELPA]:
+	http://elpa.gnu.org/packages/flymake-harper.html
 
-I am not using this package anymore, so it can be considered unmaintained.
-Patches are welcome.
+Usage
+-----
+
+You can add `flymake-harper-setup` to a hook of your choice
+(e.g. `text-mode-hook`) or by manually adding `flymake-harper-backend'
+to `flymake-diagnostic-functions'.
+
+All the analysis by Harper will then be presented using Flymake's UI.
+
+Using the user option `flymake-harper-disable' you can suppress
+certain kinds of Harper warnings.
+
+Contribute
+----------
+
+As `flymake-harper.el` is distributed as part of [GNU ELPA], and
+therefore requires a [copyright assignment] to the [FSF], for all
+nontrivial code contributions.
+
+[copyright assignment]:
+	https://www.gnu.org/software/emacs/manual/html_node/emacs/Copyright-Assignment.html
+[FSF]:
+	https://www.fsf.org/
+
+Source code
+-----------
+
+`flymake-harper` is developed on [Codeberg].
+
+[Codeberg]:
+	https://codeberg.org/pkal/flymake-harper.el
+
+Bugs and Patches
+----------------
+
+Bugs, patches, comments, or questions can be submitted via [Codeberg's
+issue system] or my sending [me] an email.
+
+[Codeberg's issue system]:
+	https://codeberg.org/pkal/flymake-harper.el/issues
+[me]:
+	https://sdf.org/~pkal/#contact
+
+Distribution
+------------
+
+`flymake-harper.el` and all other source files in this directory are
+distributed under the [GNU Public License], Version 3 (like Emacs
+itself).
+
+[GNU Public License]:
+	https://www.gnu.org/licenses/gpl-3.0.en.html
---
+++ flymake-harper.el
@@ -0,0 +1,199 @@
+;;; flymake-harper.el --- Flymake backend for harper -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021-2026  Free Software Foundation, Inc.
+
+;; Author: Manuel Uberti <manuel.uberti@inventati.org>,
+;;         Philip Kaludercic <philipk@posteo.net>
+;; Maintainer: Philip Kaludercic <philipk@posteo.net>
+;; Version: 0.1.0
+;; Keywords: convenience
+;; Package-Requires: ((emacs "27.1"))
+;; URL: https://codeberg.org/pkal/flymake-harper.el
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package adds support for harper (https://writewithharper.com/)
+;; for Flymake.  This package is a fork of Manuel Uberti's
+;; "flymake-proselint".  Once installed, the backend can be enabled
+;; with by calling `flymake-harper-setup' manually or using a hook.
+;; Configure `flymake-harper-disable' to disable certain types of
+;; warnings, if you are experiencing too many false positives.
+
+;;; Code:
+
+(eval-when-compile
+  (require 'subr-x)
+  (require 'pcase))
+(require 'flymake)
+(require 'xdg)
+
+(defgroup flymake-harper ()
+  "Flymake backend for harper."
+  :prefix "flymake-harper-"
+  :link '(url-link "https://writewithharper.com/")
+  :group 'flymake)
+
+(defcustom flymake-harper-executable "harper-cli"
+  "Name of the Harper executable.
+Note that this is not the name of the LSP server, but the standalone CLI
+program."
+  :type 'string)
+
+(defcustom flymake-harper-message-format
+  "%m%s"
+  "A format string to generate diagnostic messages.
+The following %-sequences are replaced:
+
+  %m - the message text
+  %s - suggestions, in parentheses preceded by a space 
+  %c - the error code
+  %p - numerical priority"
+  :type 'string)
+
+(defconst flymake-harper--options
+  (eval-when-compile
+    (ignore-errors
+      ;; FIXME: If harper is not installed, this will be nil until the
+      ;; package is re-compiled.
+      (with-temp-buffer
+	(call-process flymake-harper-executable nil t nil "config")
+	(goto-char (point-min))
+	(let ((config (json-parse-buffer :object-type 'alist)))
+	  (mapcar #'car config)))))
+  "List of Harper options.")
+
+(defconst flymake-harper--custom-type
+  `(set :greedy t
+        ,@(mapcar
+           (lambda (opt)
+             ;; TODO: Add a :tag
+             `(const ,opt))
+           flymake-harper--options))
+  "Custom option type for harper configurations.")
+
+(defun flymake-harper-safe-option-p (val)
+  "Check if VAL is a safe (and valid) local value."
+  (and (listp val)
+       (catch 'fail
+         (dolist (elem val)
+           (unless (memq elem flymake-harper--options)
+             (throw 'fail nil)))
+         t)))
+
+(defcustom flymake-harper-disable '(Dashes)
+  "List of Harper options to disable.
+See `flymake-harper--options' for a list of possible options.  This can
+also be a buffer local variable that you can configure using file local
+variables."
+  :safe #'flymake-harper-safe-option-p
+  :type flymake-harper--custom-type)
+
+(defun flymake-harper-sentinel-1 (source data)
+  "Handle a successfully parsed DATA from SOURCE.
+DATA is a list of error diagnostics that are converted into
+Flymake diagnostic objects."
+  (let (diags)
+    (dolist (lint data)
+      (let ((rule (plist-get lint :rule))
+	    (span (plist-get lint :span)))
+	(unless (memq rule flymake-harper-disable)
+	  (push (flymake-make-diagnostic
+		 source
+		 (1+ (plist-get span :char_start))
+		 (1+ (plist-get span :char_end))
+		 (pcase (plist-get lint :priority)
+		   ;; FIXME: I am not sure if these are the actual
+		   ;; thresholds, we should ask if around.
+		   ((pred (< 50)) :error)
+		   ((pred (< 25)) :warning)
+		   (_ :note))
+		 (format-spec
+		  flymake-harper-message-format
+		  `((?m . ,(plist-get lint :message))
+                    (?c . ,(plist-get lint :rule))
+                    (?s . ,(and-let* ((sug (plist-get lint :suggestions)))
+			     (concat " (" (string-join sug ", ") ")")))
+		    (?p . ,(plist-get lint :priority)))))
+		diags))))
+    diags))
+
+(defvar-local flymake-harper--flymake-proc nil)
+
+(defun flymake-harper-make-sentinel (source report-fn)
+  "Create a sentinel on the buffer SOURCE that will call REPORT-FN."
+  (lambda (proc _even)
+    "Sentinel on PROC for handling Harper response.
+ A successfully parsed message is passed onto the function
+ `flymake-harper-sentinel-1' for further handling."
+    (pcase (process-status proc)
+      ('exit
+       (unwind-protect
+           (when (buffer-live-p (current-buffer))
+             (with-current-buffer (process-buffer proc)
+               (goto-char (point-min))
+               (cond
+                ((with-current-buffer source
+                   (not (eq proc flymake-harper--flymake-proc)))
+                 (flymake-log :warning "Canceling obsolete check %s" proc))
+                ((= (point-max) (point-min))
+                 (flymake-log :debug "Empty response"))
+                ((condition-case err
+                     (let ((response (json-parse-buffer :object-type 'plist
+                                                        :array-type 'list)))
+		       (thread-last
+                         (plist-get (car response) :lints)
+                         (flymake-harper-sentinel-1 source)
+                         (funcall report-fn)))
+                   (json-parse-error
+                    (flymake-log :error "Invalid response: %S" err)))))))
+         (with-current-buffer source
+           (setq flymake-harper--flymake-proc nil))
+         (kill-buffer (process-buffer proc))))
+      ('signal (kill-buffer (process-buffer proc))))))
+
+(defun flymake-harper-backend (report-fn &rest _args)
+  "Flymake backend for Harper.
+REPORT-FN is the flymake reporter function.  See the Info
+node `(flymake) Backend functions' for more details."
+  (unless (executable-find flymake-harper-executable)
+    (user-error "Executable harper not found in $PATH"))
+
+  (when (process-live-p flymake-harper--flymake-proc)
+    (kill-process flymake-harper--flymake-proc))
+
+  (let ((proc (make-process
+               :name "harper-flymake" :noquery t :connection-type 'pipe
+               :buffer (generate-new-buffer " *harper-flymake*")
+               :command
+               (list flymake-harper-executable "lint" "--format=json")
+               :sentinel (flymake-harper-make-sentinel (current-buffer) report-fn)
+	       :stderr (get-buffer-create " *harper-flymake-stderr*"))))
+    (setq flymake-harper--flymake-proc proc)
+    (save-restriction
+      (widen)
+      (process-send-region proc (point-min) (point-max))
+      (process-send-eof proc))))
+
+(defun flymake-harper-setup ()
+  "Set up the Harper as a Flymake backend in this buffer.
+The Flymake minor mode will be enabled if it is not already enabled.."
+  (interactive)
+  (unless flymake-mode
+    (flymake-mode t))
+  (add-hook 'flymake-diagnostic-functions #'flymake-harper-backend nil t))
+
+(provide 'flymake-harper)
+;;; flymake-harper.el ends here

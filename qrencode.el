60cc1b9d0a7a0026c20a7446072cf94e3b1f63c7
Rüdiger Sonderfeld <ruediger@c-plusplus.net>
2026-09-24 23:14:19 +0100

    Release 1.5.
    * Various: improvements on capacity, mask selection, and removal of a
      spurious byte.
    * Due: to this QR Codes generated with 1.5 can differ from previous
      version!
    * Support: for P4 NetPBM format with a much faster exporter! Customise
      `qrencode-export-format` to use it.
    * `qrencode-format-as-netpbm`: is obsolete as P1 NetPBM support will
      be removed in the next major release.

--- qrencode.el
+++ qrencode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Rüdiger Sonderfeld <ruediger@c-plusplus.net>
 ;; Keywords: qrcode comm
-;; Version: 1.5-beta4
+;; Version: 1.5
 ;; Package-Requires: ((emacs "25.1"))
 ;; Package: qrencode
 ;; URL: https://github.com/ruediger/qrencode-el
